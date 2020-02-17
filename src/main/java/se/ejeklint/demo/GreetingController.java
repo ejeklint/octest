1b50a82cc74f3389ed47bfae0c7de5935038fe82
Per Ejeklint <per.ejeklint@arbetsformedlingen.se>
2020-02-17 15:24:15 +0100

    first commit

---
+++ src/main/java/se/ejeklint/demo/GreetingController.java
@@ -0,0 +1,19 @@
+package se.ejeklint.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+
+    @GetMapping("/")
+    public String rootGreeting() {
+        return "Root-saker äro nyttiga!";
+    }
+    
+    @GetMapping("/greeting")
+    public String greeting(@RequestParam(value = "name", defaultValue = "Värld") String name) {
+        return name;
+    }
+}
